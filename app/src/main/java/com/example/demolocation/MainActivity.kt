6c18572d9545c3ec178bf69398b7b50c76b6b073
Govind Gupta <govind.it.20016@recb.ac.in>
2023-08-15 19:24:53 +0530

    first commit

---
+++ app/src/main/java/com/example/demolocation/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.example.demolocation
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.example.demolocation.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import java.util.Locale
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mfusedLocationClient: FusedLocationProviderClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mfusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        binding.btnGetLocation.setOnClickListener {
+            getlocation()
+        }
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun getlocation() {
+        if (checkPermission()) {
+            if (isLocationEnabled()) {
+                mfusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    val location: Location? = task.result
+                    if (location != null) {
+                        val geocoder = Geocoder(this, Locale.getDefault())
+                        val list: List<Address> = geocoder.getFromLocation(
+                            location.latitude,
+                            location.latitude,
+                            1
+                        ) as List<Address>
+                        binding.apply {
+                            txtLat.text = "Latitude\n${list[0].latitude}"
+                            txtLng.text = "Longitude\n${list[0].longitude}"
+                            countryName.text = "Country Name\n${list[0].countryName}"
+                            txtLocality.text = "Locality\n${list[0].locality}"
+                            txtAddress.text = "Address\n${list[0].getAddressLine(0)}"
+                        }
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please turn on your location", Toast.LENGTH_SHORT).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermission()
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ),2
+        )
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun checkPermission(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+}
